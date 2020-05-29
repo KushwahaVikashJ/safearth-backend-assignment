888c8f20796f8a3a96ddcc47cf0a8cee900e0fb7
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-05-29 19:10:45 +0530

    updated

--- app.js
+++ app.js
@@ -39,4 +39,4 @@ app.get('/',(req,res)=>{
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>winston.info(`Listening on port ${port}....`));
+app.listen(port,()=>winston.info(`Listening on PORT ${port}....`));
