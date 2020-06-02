d97420a090199e05cd1b02f2b4f4cdd9937adfaf
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-06-02 18:34:02 +0530

    done

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/',(req,res)=>{
     res.send('Hello');
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listening On PORT ${port}....`));
