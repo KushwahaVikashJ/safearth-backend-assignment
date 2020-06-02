ace030c8921eb4bdc7b5217fb93fe6537a8bd403
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-06-02 18:31:46 +0530

    done

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const helmet = require('helmet');
-const compression =  require('compression');
 const express = require('express');
 const app = express();
 
-app.use(helmet());
-app.use(compression());
-
+require('./startup/routes')(app);
 require('./startup/config')();
 require('./startup/db')();
 require('./startup/logging')();
-require('./startup/routes')();
 
 app.get('/',(req,res)=>{
     res.send('Hello');
